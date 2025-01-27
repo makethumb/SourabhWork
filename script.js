c80c77f80c0d9f9e192e9b9db42c6a9dde95bf44
MakeThumb <carryonsrb@gmail.com>
2025-01-28 00:25:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// Add any JavaScript for interactive elements here if needed.
-    const elements = document.querySelectorAll('.animate-left');
-        elements.forEach(element => {
-          element.classList.add('animate__animated');
-          element.classList.add('animate__fadeInLeft');
-        });
+
